9e582b143530c7ded4f0f275a1596a7b4e62628c
Alexander Pabon <alex312-@hotmail.com>
2021-09-25 15:44:55 -0500

    The DetailProduct component is implemented

--- src/components/DetailProduct.js
+++ src/components/DetailProduct.js
@@ -1,10 +1,28 @@
 import React from "react";
+import image from "./../assets/audifonos.png"
 
 export default class DetailProduct extends React.Component {
   render() {
     return (
       <div className="detail-product">
-        <p>Detail Product</p>
+        <p>Detail</p>
+        <div className="row">
+          <div className="col-6">
+            <img src={image} className="img-detail-product" alt="imagen producto mercado libre" />
+          </div>
+          <div className="col-4 margin-top-large">
+            <div className="font-size-14 margin-bottom-small">Cantidad</div>
+            <div className="font-size-24 margin-bottom-large">Titulo</div>
+            <div className="font-size-46 margin-bottom-large">Precio</div>
+            <button type="submit" className="font-size-16">Comprar</button>
+          </div>
+        </div>
+        <div className="row margin-left-large margin-bottom-large">
+          <div className="col-12 font-size-28">Descripcion</div>
+        </div>
+        <div className="row margin-left-large margin-bottom-large">
+          <div className="col-12 font-size-16">Resumen</div>
+        </div>
       </div>
     );
   }
